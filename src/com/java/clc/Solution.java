9e657d85cdf5ed02e5958ec281300022e53d8976
leontev07 <leontev07@mail.ru>
2022-07-15 18:21:54 +0300

    Добавил ветку

--- src/com/java/clc/Solution.java
+++ src/com/java/clc/Solution.java
@@ -1,4 +1,13 @@
 package com.java.clc;
 
+
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+    }
+
 }
---
+++ src/com/java/clc/test10/Homework1.java
@@ -0,0 +1,11 @@
+package com.java.clc.test10;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        main2();
+    }
+
+    public static void main2() {
+        System.out.println("Ларин");
+    }
+}
